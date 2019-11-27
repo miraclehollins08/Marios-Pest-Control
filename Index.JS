0ce24473896e341cf4fe59a8b74a84b960b5727f
Miracle <54377927+miraclehollins08@users.noreply.github.com>
2019-11-26 21:14:23 -0900

    Add files via upload

---
+++ Index.JS
@@ -0,0 +1,9 @@
+function sumValues()
+{
+    var num1, num1, res;
+num1=Number(document.formcalc.txtnum1.value);
+num2=Number(document.formcalc.txtnum2.value);
+num3=Number(document.formcalc.txtnum3.value);
+res=num1+num2+num3;
+document.formcalc.txtres.value=res;    
+}
